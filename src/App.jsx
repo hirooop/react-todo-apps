8deba46398e7eaa044d3e362b577178fe73e5b3f
hirooop <93263419+hirooop@users.noreply.github.com>
2022-02-15 21:19:36 +0900

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import "./styles.css";
+import { InputTodo } from "./Componets/InputTodo";
+import { InCompleteTodos } from "./Componets/InCompleteTodos";
+import { CompleteTodos } from "./Componets/CompleteTodos";
 
 export const App = () => {
   const [todoText, setTodoText] = useState([""]);
-  const [incompleteTodos, setIncompleteTodos] = useState(["", ""]);
+  const [incompleteTodos, setIncompleteTodos] = useState([""]);
   const [completeTodos, setCompleteTodos] = useState([""]);
   const onChangeTodotext = (event) => setTodoText(event.target.value);
   const onClickAdd = () => {
@@ -34,41 +37,21 @@ export const App = () => {
 
   return (
     <>
-      <div className="input-area">
-        <input
-          placeholder="TODOを入力"
-          value={todoText}
-          onChange={onChangeTodotext}
-        />
-        <button onClick={onClickAdd}>追加</button>
-      </div>
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="complete-area">
-        <p className="title">完了のTODO</p>
-        <ul>
-          {completeTodos.map((todo, index) => {
-            return (
-              <div className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickBack(index)}>戻す</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodotext}
+        onClick={onClickAdd}
+        disabled={incompleteTodos.length > 4}
+      />
+      {incompleteTodos.length > 4 && (
+        <p style={{ color: "red" }}>登録できるTODOは5個までです。</p>
+      )}
+      <InCompleteTodos
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
     </>
   );
 };
